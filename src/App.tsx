6e683109c90de70a5b9c78d60e951912fddf3b3b
berkeralanli <147711608+berkeralanli@users.noreply.github.com>
2023-11-17 16:40:07 +0300

    navbar-flex-between

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import { Box, createTheme } from "@mui/material";
-import { CssBaseline, ThemeProvider} from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { themeSettings } from "./theme"
+import { Box } from "@mui/material";
 import Navbar from "@/scenes/navbar";
 
-function App() {
-  const theme = useMemo(() => createTheme(themeSettings), []);
-  return (
-      <div className="app">
-        <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-            <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
-              <Route path="/predictions" element={<div>prediction page</div>} />
-            </Routes>
-          </Box>
-        </ThemeProvider>
-        </BrowserRouter>
-
-      </div>
-  )
+function App(){
+  const theme = useMemo(() => createTheme(themeSettings), [])
+  return <div className="app">
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<div>dashboard page</div>} />
+          <Route 
+          path="/predictions" 
+          element={<div>prediction page</div>} />
+          </Routes>
+        </Box>
+    </ThemeProvider>
+    </BrowserRouter>
+  </div>
 }
-
-export default App
+export default App;
---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Box, Typography, useTheme } from '@mui/material';
+import FlexBetween from '@/components/FlexBetween';
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  const { palette } = useTheme(); 
+  return (
+    <FlexBetween 
+    mb="0.25rem" 
+    p="0.5rem 0 rem" 
+    color={palette.grey[300]}
+    > Hey </FlexBetween>
+  );
+};
+
+export default Navbar;
