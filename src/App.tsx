bd12b6f692f4d8fd06cea5706193de51984a81a8
berkeralanli <147711608+berkeralanli@users.noreply.github.com>
2023-11-18 17:54:11 +0300

    grid-olusumu-ve-navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import Navbar from "@/scenes/navbar";
-
+import Dashboard from "@/scenes/dashboard"
 function App(){
   const theme = useMemo(() => createTheme(themeSettings), [])
   return <div className="app">
@@ -15,10 +15,10 @@ function App(){
         <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
         <Navbar />
         <Routes>
-          <Route path="/" element={<div>dashboard page</div>} />
+          <Route path="/" element={<Dashboard/>} />
           <Route 
-          path="/predictions" 
-          element={<div>prediction page</div>} />
+          path="/tahminleme" 
+          element={<div>tahminleme sayfasi</div>} />
           </Routes>
         </Box>
     </ThemeProvider>
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 import { Box, Typography, useTheme } from '@mui/material';
 import FlexBetween from '@/components/FlexBetween';
 
@@ -7,12 +8,53 @@ type Props = {}
 
 const Navbar = (props: Props) => {
   const { palette } = useTheme(); 
+  const [selected, setSelected] = useState("dashboard");
   return (
+    // sol taraf 
+
     <FlexBetween 
     mb="0.25rem" 
     p="0.5rem 0 rem" 
     color={palette.grey[300]}
-    > Hey </FlexBetween>
+    > <FlexBetween gap="0.75rem">
+        <AcUnitIcon sx={{fontSize:"28px"}}/>
+        <Typography variant='h4' fontSize="16px">
+          RetroLuks
+        </Typography>
+      </FlexBetween> 
+      <FlexBetween gap="2rem">
+        <Box sx={{"&:hover": {
+          color: palette.primary[100]
+        }}}>
+          <Link
+          to="/"
+          onClick={() => setSelected("dashboard")}
+          style={{
+            color:selected === "dashboard" ? "inherit" : palette.grey[700],
+            textDecoration: "inherit"
+          }}>
+            dashboard
+          </Link>
+        </Box>
+        <Box sx={{"&:hover": {
+          color: palette.primary[100]
+        }}}>
+          <Link
+          to="/tahminleme"
+          onClick={() => setSelected("tahminleme")}
+          style={{
+            color:selected === "tahminleme" ? "inherit" : palette.grey[700],
+            textDecoration: "inherit"
+          }}>
+            tahminleme
+          </Link>
+        </Box>
+
+
+      </FlexBetween>
+
+      
+      </FlexBetween>
   );
 };
 
