33543ea0f781ee241aeaf178d0fc9f0624bdb93a
berkeralanli <147711608+berkeralanli@users.noreply.github.com>
2023-11-25 21:21:41 +0300

    c

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,6 +1,16 @@
 import DashboardBox from '@/components/DashboardBox';
 import  { useGetKpisQuery } from '@/state/api';
 import React from 'react';
+import { 
+  AreaChart,
+  ResponsiveContainer,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Area
+
+} from "recharts";
 
 type Props = {}
 
@@ -9,6 +19,26 @@ const Row1 = (props: Props) => {
   return (
     <>
     <DashboardBox gridArea="a"></DashboardBox>
+    <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={data}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Area 
+          type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8"/>
+        </AreaChart>
+      </ResponsiveContainer>
     <DashboardBox gridArea="b"></DashboardBox>
     <DashboardBox gridArea="c"></DashboardBox>
     </>
