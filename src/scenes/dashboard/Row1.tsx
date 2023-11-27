a1746bf16172f85c6ed7ad17fa42867a98c12809
berkeralanli <147711608+berkeralanli@users.noreply.github.com>
2023-11-28 00:00:18 +0300

    types

--- src/scenes/dashboard/Row1.tsx
+++ src/scenes/dashboard/Row1.tsx
@@ -1,24 +1,16 @@
 import DashboardBox from '@/components/DashboardBox';
 import  { useGetKpisQuery } from '@/state/api';
-import React from 'react';
-import { 
-  AreaChart,
-  ResponsiveContainer,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Area
-
-} from "recharts";
-
+import { useTheme } from '@mui/material';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 type Props = {}
 
 const Row1 = (props: Props) => {
- const { data } = useGetKpisQuery();
+  const { palette } = useTheme();
+  // const { data } = useGetKpisQuery();
+  // console.log("data:", data);
   return (
     <>
-    <DashboardBox gridArea="a"></DashboardBox>
+    <DashboardBox gridArea="a">
     <ResponsiveContainer width="100%" height="100%">
         <AreaChart
           width={500}
@@ -31,14 +23,25 @@ const Row1 = (props: Props) => {
             bottom: 0,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
+          <defs>
+            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5}/>
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0}/>
+            </linearGradient>
+          </defs>
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
           <Area 
-          type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8"/>
+          type="monotone" 
+          dataKey="uv"stroke={palette.primary.main} 
+          fillOpacity={1} 
+          fill="url(#colorRevenue)"/>
         </AreaChart>
       </ResponsiveContainer>
+    </DashboardBox>
+    
+    
     <DashboardBox gridArea="b"></DashboardBox>
     <DashboardBox gridArea="c"></DashboardBox>
     </>
---
+++ src/state/types.ts
@@ -0,0 +1,12 @@
+
+export interface GetKpisResponse{
+  Country: string;
+  Customer_ID: number;
+  Description: string;
+  Invoice_ID: number;
+  Price: number;
+  Quantity: number;
+  StockCode: string;
+  __v: number;
+  InvoiceDate: string;
+}
