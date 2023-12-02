20155a13551080c51c45829ed2dc5bd1a70f0dcb
berkeralanli <147711608+berkeralanli@users.noreply.github.com>
2023-12-02 21:38:48 +0300

    abcde

--- src/scenes/dashboard/Row2.tsx
+++ src/scenes/dashboard/Row2.tsx
@@ -1,139 +1,217 @@
+import DashboardBox from '@/components/DashboardBox';
+import Boxheader from '@/components/BoxHeader';
+import  { useGetMostSellers2022Query, useGetMostSellers2023Query } from '@/state/api';
+import { Box, Typography, useTheme } from '@mui/material';
+import { useMemo } from 'react';
+import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
+import FlexBetween from '@/components/FlexBetween';
 import BoxHeader from '@/components/BoxHeader';
-import DashboardBox from '@/components/DashboardBox'
-import YearSelector from '@/components/yearSelector';
-import { useGetMonthlyProfit2022Query, useGetMonthlyProfit2023Query } from '@/state/api';
-import { Typography, useTheme } from '@mui/material';
-import { useMemo, useState } from 'react'
-import { AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis,  } from 'recharts';
 
+const pieData = [
+  {name: "Group A", value: 700},
+  {name: "Group B", value: 300},
+ 
+  
+]
 type Props = {}
-function getMonthName(monthNumber) {
-  const monthNames = [
-    "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
-    "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"
-  ];
-  return monthNames[monthNumber - 1];
-}
 
-const Row2 = () => {
+const Row1 = () => {
   const { palette } = useTheme();
-  const [selectedYear, setSelectedYear] = useState(2022);
+  const pieColors = [palette.primary[800], palette.primary[600]];
+  const { data: data2022 } = useGetMostSellers2022Query();
+  const { data: data2023 } = useGetMostSellers2023Query();
 
-  const handleYearSelect = (year) => {
-    setSelectedYear(year);
-  };
-
-  const { data: monthlyData2022 } = useGetMonthlyProfit2022Query();
-  const { data: monthlyData2023 } = useGetMonthlyProfit2023Query();
-
-  const transformedMonthlyData = useMemo(() => {
-    const monthlyData = selectedYear === 2022 ? monthlyData2022 : monthlyData2023;
-
-    if (monthlyData) {
-      return monthlyData.map((item) => ({
-        month: getMonthName(item.month).substring(0, 3),
-        ToplamGelir: item.totalRevenue,
-        ToplamAdet: item.totalQuantity.toLocaleString("tr-TR"),
+  const transformedData2022 = useMemo(() => {
+    if (data2022) {
+      return data2022.map((item) => ({
+        id: item._id, // Örnek olarak _id kullanıldı, gerçek verilere göre düzenlenmeli
+        name: item.ProductDescription.length > 20 ? item.ProductDescription.slice(0, 20) + '...' : item.ProductDescription,
+        Gelir: item.totalRevenue,
+        Adet: item.totalQuantity,
       }));
     }
     return [];
-  }, [monthlyData2022, monthlyData2023, selectedYear]);
-  
+  }, [data2022]);
+
+  const transformedData2023 = useMemo(() => {
+    if (data2023) {
+      return data2023.map((item) => ({
+        id: item._id, // Örnek olarak _id kullanıldı, gerçek verilere göre düzenlenmeli
+        name: item.ProductDescription.length > 20 ? item.ProductDescription.slice(0, 20) + '...' : item.ProductDescription,
+        Gelir: item.totalRevenue,
+        Adet: item.totalQuantity,
+      }));
+    }
+    return [];
+  }, [data2023]);
+
   return (
     <>
-    <DashboardBox gridArea="d">
-   <YearSelector onSelectYear={handleYearSelect}></YearSelector>
-    <BoxHeader 
-    title='Aylara Göre Satış Toplamları' 
-    subtitle='Grafik satışların ay bazlı gruplanıp toplanmasıyla elde ediliyor' 
+    <DashboardBox gridArea="d" >
+    <BoxHeader
+    title='2022 Yılı' 
+    subtitle='En Çok Ciro Sağlayan 3 ürün' 
     sideText='0.24%'/>
     <ResponsiveContainer width="100%" height="80%">
-        <BarChart
+      
+        <AreaChart
           width={500}
           height={400}
-          data={transformedMonthlyData}
+          data={transformedData2022}
           margin={{
-            top: 10,
-            right: 10,
-            left: 0,
-            bottom: 5,
+            top: 15,
+            right: 16,
+            left: -4,
+            bottom: -10,
           }}
         >
           <defs>
             <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.8}/>
-                <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0}/>
-              </linearGradient>
-              
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5}/>
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0}/>
+            </linearGradient>
+            <linearGradient id="colorQuantity" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5}/>
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0}/>
+            </linearGradient>
           </defs>
-          <CartesianGrid vertical={false} stroke={palette.grey[800]}/>
-          <XAxis dataKey="month" axisLine={false} tickLine={false} style={{ fontSize: "10px"}}/>
-          <YAxis  axisLine={false} tickLine={false} style={{ fontSize: "10px"}} />
-          <Tooltip />
-          <Bar dataKey="ToplamGelir" fill="url(#colorRevenue)" />
-        </BarChart>
-      </ResponsiveContainer>
-    </DashboardBox>
-  
-    
-    <DashboardBox gridArea="e">
-    <BoxHeader
-    title='Yılı  ' 
-    subtitle='Toplam gelir ve Toplam adetin gösterimi' 
-    sideText='0.24%'/>
-    <ResponsiveContainer width="100%" height="80%">
-      
-        <LineChart
-          width={500}
-          height={400}
-          data={transformedMonthlyData}
-          margin={{
-            top: 20,
-            right: 0,
-            left: -10,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid vertical={false} stroke={palette.grey[800]} />
           <XAxis 
-          dataKey="month"
+          dataKey="name"
           tickLine={false}
           style={{ fontSize:"8px"}} />
           <YAxis 
-          yAxisId="left"
           tickLine={false}
-          axisLine={false}
-          style={{ fontSize: "10px"}}
-          />
-          <YAxis 
-          yAxisId="right"
-          orientation='right'
-          tickLine={false}
-          axisLine={false}
-          style={{ fontSize: "10px"}}
-          />
+          axisLine={{strokeWidth:"0"}}
+          style={{ fontSize:"10px"}}
+          domain={[10000,175000]}/>
           
           <Tooltip />
-          <Legend height={10} wrapperStyle={{
-            margin: '0 0 10px 0 '
-          }}/>
-
-          <Line 
-          yAxisId="left"
-          type="monotone"
-          dataKey="ToplamGelir"
-          stroke={palette.tertiary[500]}/>
-          <Line 
-          yAxisId="right"
-          type="monotone"
-          dataKey="ToplamAdet"
-          stroke={palette.primary.main}/>
-        </LineChart>
+          <Area 
+          type="monotone" 
+          dataKey="Gelir"
+          dot={true}
+          stroke={palette.primary.main} 
+          fillOpacity={1} 
+          fill="url(#colorRevenue)"/>
+          <Area 
+          type="monotone" 
+          dataKey="Adet"
+          dot={true}
+          stroke={palette.primary.main} 
+          fillOpacity={1} 
+          fill="url(#colorQuantity)"/>
+        </AreaChart>
       </ResponsiveContainer>
     </DashboardBox>
-    <DashboardBox gridArea="f"></DashboardBox>
+    
+    
+    <DashboardBox gridArea="e">
+    <BoxHeader
+    title='2023 Yılı' 
+    subtitle='En Çok Ciro Sağlayan 5 ürün' 
+    sideText='0.24%'/>
+    <ResponsiveContainer width="100%" height="80%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={transformedData2023}
+          margin={{
+            top: 15,
+            right: 16,
+            left: -4,
+            bottom: -10,
+          }}
+        >
+          <defs>
+            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5}/>
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0}/>
+            </linearGradient>
+            <linearGradient id="colorQuantity" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5}/>
+              <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0}/>
+            </linearGradient>
+          </defs>
+          <XAxis 
+          dataKey="name"
+          tickLine={false}
+          style={{ fontSize:"8px"}} />
+         <YAxis 
+          tickLine={false}
+          axisLine={{strokeWidth:"0"}}
+          style={{ fontSize:"10px"}}
+          domain={[10000,350000]}/>
+          
+          <Tooltip />
+          <Area 
+          type="monotone" 
+          dataKey="Gelir"
+          dot={true}
+          stroke={palette.primary.main} 
+          fillOpacity={1} 
+          fill="url(#colorRevenue)"/>
+          <Area 
+          type="monotone" 
+          dataKey="Adet"
+          dot={true}
+          stroke={palette.primary.main} 
+          fillOpacity={1} 
+          fill="url(#colorQuantity)"/>
+        </AreaChart>
+      </ResponsiveContainer>
+    </DashboardBox>
+
+
+
+
+    <DashboardBox gridArea="f">
+      <Boxheader title="2022 ve 2023 yılları" subtitle='En çok satan 2 ürünün karşılaştırması' sideText='0.24%'/>
+      <FlexBetween mt="1rem" gap="1.5rem" pr="1rem">
+    <PieChart 
+    width={100} 
+    height={150}
+    margin={{
+      top: 0,
+      right: -10,
+      left: 10,
+      bottom: 0,
+    }}
+    >
+        <Pie
+          stroke='none'
+          data={pieData}
+          innerRadius={18}
+          outerRadius={38}
+          paddingAngle={2} 
+          dataKey="value"
+        >
+          {pieData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={pieColors[index]} />
+          ))}
+        </Pie>
+       
+      </PieChart>
+        
+        <Box ml="-0.7rem" flexBasis={"40%"} textAlign={"center"}>
+          <Typography variant="h4">Fark</Typography>
+          <Typography m="0.5rem 0 " variant="h3" color={palette.primary[300]}>%70</Typography>
+        <Typography variant="h6">Şirket geçen sene en çok kar sağlayan ürüne göre değişim yakalamıştır.</Typography>
+        
+        </Box>
+        <Box ml="-0.7rem" flexBasis={"40%"} textAlign={"center"}>
+          <Typography variant="h5">Adet Bazlı Satışlarda</Typography>
+          <Typography variant="h6">Satılan toplam adet</Typography>
+          <Typography m="0.4rem" variant="h5"> Ciro karşılaştırması </Typography>
+        <Typography variant="h6">Kar oranı %109'a kadar çıkmıştır.</Typography>
+
+        </Box>
+        
+      </FlexBetween>
+    </DashboardBox>
+
+    
     </>
   )
 }
 
-export default Row2
+export default Row1
